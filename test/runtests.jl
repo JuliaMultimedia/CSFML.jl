d24bdcd62af443d63e0d51ceb71ac2f3d04a5768
Gnimuc <qiyupei@gmail.com>
2021-05-11 19:15:41 +0900

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,4 +4,5 @@ using Test
 @testset "version" begin
     @test LibCSFML.CSFML_VERSION_MAJOR == 2
     @test LibCSFML.CSFML_VERSION_MINOR == 5
+    @test_nowarn mode = sfVideoMode(1280, 720, 32)
 end
